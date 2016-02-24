1d84d4511d220d710dbdee2fe16b196f3abb36a9
ianazol <yana.kolchanova@gmail.com>
2016-02-24 17:50:29 +0500

    кодировка в хедере

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<script src="build/main.js" type="text/javascript"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
 </head>
 
 <body>
