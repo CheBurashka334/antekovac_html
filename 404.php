a5ee835e82b1fcfad1c93cad20132af4b612f585
myaff <grabka74@gmail.com>
2016-03-18 13:46:08 +0500

    404

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php require ('header.php') ?>
+<div class="container">
+	<ul class="breadcrumbs">
+		<li class="breadcrumbs__item"><a href="#">Главная</a></li>
+		<li class="breadcrumbs__item breadcrumbs__item--unactive"><a href="#">Ошибка 404</a></li>
+	</ul>
+</div>
+<div class="container">
+	<div class="page--404">
+		<div class="page__picture">
+			<img src="images/404.png"/>
+		</div>
+		<div class="heading">
+			<h2>страница не найдена</h2>
+		</div>
+		<a href="#" class="btn btn--arrow btn--transparent">Вернуться на главную</a>
+	</div>
+</div>
+<?php require ('footer.php') ?>
