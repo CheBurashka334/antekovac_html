9eb8ee39dcb729390b5e385dcd25f4146f173d71
myaff <grabka74@gmail.com>
2016-04-06 12:14:44 +0500

    статья деталка

---
+++ article_detail.php
@@ -0,0 +1,129 @@
+<?php require ('header.php') ?>
+<div class="container">
+	<ul class="breadcrumbs">
+		<li class="breadcrumbs__item"><a href="#">Главная</a></li>
+		<li class="breadcrumbs__item"><a href="#">Бренд</a></li>
+		<li class="breadcrumbs__item"><a href="#">Статьи</a></li>
+		<li class="breadcrumbs__item breadcrumbs__item--unactive"><a href="#">Название статьи</a></li>
+	</ul>
+</div>
+<div class="container page--article">
+	<div class="grid-equalHeight">
+		<div class="col-4_sm-12">
+			<div class="tile-block tile-block--collection tile-block--collection--big mobile-fullwidth">
+                <div class="tile-block__wrapper" style="background-image: url(images/collection-circus_bg.jpg)">
+                    <div class="collection__info">
+						<div class="collection__icon">
+							<img src="images/collection-circus_icon.png"/>
+						</div>
+						<div class="collection__heading">
+							Что говорят
+						</div>
+						<div class="collection__note">
+							Мы публикуем статьи, интервью и материалы о нас в СМИ
+						</div>
+						<div class="collection__button">
+							<a href="collection_process.php" class="btn btn--transparent btn--arrow">
+								Смотреть что о нас говорят
+							</a>
+						</div>
+					</div>
+                </div>
+            </div>
+		</div>
+		<div class="col-8_sm-12">
+			<div class="tile-block tile-block--scrollable mobile-fullwidth">
+				<div class="tile-block__heading tile-block__heading--date">
+					<h2>название статьи</h2>
+					<div class="tile-block__note news__date">12 марта 2016 г.</div>
+				</div>
+				<div class="tile-block__content js-scrollable-content" data-height="27em">
+					<p>Бренд Ante Kovac – это новое решение в мире моды, стиля и утонченности. Нашими клиентами становятся успешные люди, которые стремятся поразить окружающих своим изысканным и тонким вкусом. Среди приоритетных задач AnteKovac – нахождение новых способов самовыражения и создание сумок и аксессуаров в обход шаблонных стереотипов. Абсолютно все дизайнерские сумки Анте Ковач – изделия ручной работы с тщательно продуманными деталями, рисунки также наносятся вручную. Художник вкладывает свою душу, а вещь сохраняет теплоту мастерских рук.</p>
+					<p>АнтеКовач занимается производством концептуальных, эксклюзивных вещей для тех, кто не приемлет оставаться незамеченным, а предпочитает выделяться из общей массы и выглядеть так, как никто другой. Идеология стиля Ante Kovac основывается на сочетании незаурядных современных форм с изыском художественных уникальных и оригинальных дизайнерских разработок, свой стиль мы называем «ANTEкварный casual”.</p>
+					<p>Наша продукция пользуется спросом у неординарных личностей, которые мыслят смело и не боятся показать свои чувства. Эти люди хотят быть неповторимыми и получать от жизни максимум удовольствия, выражая независимость своего мышления и вкуса.</p>
+					<p>Успех компании начался с пошива сумок ручной работы на заказ и их продаж в интернет-магазине; по сей день соблюдается особая производственная традиция – все модные сумки мы создаем исключительно из натуральной кожи и вручную.</p>
+					<p>Бренд Ante Kovac – это новое решение в мире моды, стиля и утонченности. Нашими клиентами становятся успешные люди, которые стремятся поразить окружающих своим изысканным и тонким вкусом. Среди приоритетных задач AnteKovac – нахождение новых способов самовыражения и создание сумок и аксессуаров в обход шаблонных стереотипов. Абсолютно все дизайнерские сумки Анте Ковач – изделия ручной работы с тщательно продуманными деталями, рисунки также наносятся вручную. Художник вкладывает свою душу, а вещь сохраняет теплоту мастерских рук.</p>
+					<p>АнтеКовач занимается производством концептуальных, эксклюзивных вещей для тех, кто не приемлет оставаться незамеченным, а предпочитает выделяться из общей массы и выглядеть так, как никто другой. Идеология стиля Ante Kovac основывается на сочетании незаурядных современных форм с изыском художественных уникальных и оригинальных дизайнерских разработок, свой стиль мы называем «ANTEкварный casual”.</p>
+					<p>Наша продукция пользуется спросом у неординарных личностей, которые мыслят смело и не боятся показать свои чувства. Эти люди хотят быть неповторимыми и получать от жизни максимум удовольствия, выражая независимость своего мышления и вкуса.</p>
+					<p>Успех компании начался с пошива сумок ручной работы на заказ и их продаж в интернет-магазине; по сей день соблюдается особая производственная традиция – все модные сумки мы создаем исключительно из натуральной кожи и вручную.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="heading"><h2>Читать другие статьи</h2></div>
+	<div class="tile-block news-slider mobile-fullwidth">
+		<div class="grid-noGutter-center">
+			<div class="col-10_sm-12">
+				<div class="carousel--dark-navs--outside">
+					<div class="owl-carousel">
+						<div class="news-slider__slide grid-equalHeight">
+							<div class="col-5_sm-12 news-slider__img">
+								<img src="images/news-img-1.jpg"/>
+							</div>
+							<div class="col-6_sm-12 news-slider__content">
+								<div class="news-slider__title">Загловок статьи. <span class="light-color">Название журнала, октябрь 2016</span></div>
+								<div class="news-slider__text">
+									<p>Идейные соображения высшего порядка, а также постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям.</p>
+								</div>
+								<div class="news-slider__buttons">
+									<a href="#" class="btn btn--dark--medium">
+										<span class="btn-text">Читать статью</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="news-slider__slide grid-equalHeight">
+							<div class="col-5_sm-12 news-slider__img">
+								<img src="images/news-img-4.jpg"/>
+							</div>
+							<div class="col-6_sm-12 news-slider__content">
+								<div class="news-slider__title">Загловок статьи. <span class="light-color">Название журнала, октябрь 2015</span></div>
+								<div class="news-slider__text">
+									<p>Идейные соображения высшего порядка, а также постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям.</p>
+								</div>
+								<div class="news-slider__buttons">
+									<a href="#" class="btn btn--dark--medium">
+										<span class="btn-text">Читать статью</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="news-slider__slide grid-equalHeight">
+							<div class="col-5_sm-12 news-slider__img">
+								<img src="images/news-img-2.jpg"/>
+							</div>
+							<div class="col-6_sm-12 news-slider__content">
+								<div class="news-slider__title">Загловок статьи. <span class="light-color">Название журнала, октябрь 2014</span></div>
+								<div class="news-slider__text">
+									<p>Идейные соображения высшего порядка, а также постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям.</p>
+								</div>
+								<div class="news-slider__buttons">
+									<a href="#" class="btn btn--dark--medium">
+										<span class="btn-text">Читать статью</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="news-slider__slide grid-equalHeight">
+							<div class="col-5_sm-12 news-slider__img">
+								<img src="images/news-img-3.jpg"/>
+							</div>
+							<div class="col-6_sm-12 news-slider__content">
+								<div class="news-slider__title">Загловок статьи. <span class="light-color">Название журнала, октябрь 2014</span></div>
+								<div class="news-slider__text">
+									<p>Идейные соображения высшего порядка, а также постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям.</p>
+								</div>
+								<div class="news-slider__buttons">
+									<a href="#" class="btn btn--dark--medium">
+										<span class="btn-text">Читать статью</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php require ('footer.php') ?>
