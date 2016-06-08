890d0f1abb4dedf518636440f3730fff6b88f4de
myaff <grabka74@gmail.com>
2016-06-08 12:08:23 +0500

    текстовая страница
    Внимание! на текстовых страницах нужно добавить классы!

---
+++ text-page.php
@@ -0,0 +1,25 @@
+<?php require ('header.php') ?>
+<div class="container">
+	<ul class="breadcrumbs">
+		<li class="breadcrumbs__item"><a href="#">Главная</a></li>
+		<li class="breadcrumbs__item"><a href="#">Бренд</a></li>
+		<li class="breadcrumbs__item"><a href="#">Новости</a></li>
+		<li class="breadcrumbs__item breadcrumbs__item--unactive"><a href="#">Название новости</a></li>
+	</ul>
+</div>
+<div class="container page--text">
+	<div class="tile-page">
+		<div class="tile-page__image mobile-fullwidth--noGutter">
+			<img src="images/projects.jpg"/>
+		</div>
+		<div class="tile-page__body">
+			<h2>Творческие проекты</h2>
+			<br>
+			<p>
+				Мы готовы принимать участие в следующих проектах: съемки кинофильмов, телепередачи, фотосессии, театральные постановки, перфомансы, выставки современного искусства с участием предметов нашего производства или с целью привлечения специалистов нашей команды для создания креативных образов и концепций. ANTE KOVAC рассматривает возможность участия в благотворительных и некоммерческих социальных проектах. Если у вас есть идеи и предложения, пишите или звоните <a href="tel:8-800-700-75-99">8-800-700-75-99</a>.
+			</p>
+			<a href="javascript:void(0)" class="btn btn--transparent btn--arrow js-toggle-box" data-box=".page-aside.feed-back">Отправить сообщение</a>
+		</div>
+	</div>
+</div>
+<?php require ('footer.php') ?>
